d6d382e59d085d80bf1b9174bf265ffddafbd206
Arthur Katossky <arthur.katossky@free.Fr>
2017-01-20 15:42:35 +0100

    improve coverage of sheet names with wild character encoding (#33) closes #32
    * improve string coverage of sheets' names
    
    A dependance toward `stringi` is introduced, but this is the price of good string comparisons.
    
    * fix typos
    
    * update main test file
    
    * add test ODS file
    
    * update name file in tests
    
    * add same ODS file with new name
    
    * remove file with old name
    
    @katossky

--- tests/testthat/test_read_ods.R
+++ tests/testthat/test_read_ods.R
@@ -3,3 +3,8 @@ test_that("Single column ODS", {
     expect_equal(ncol(single_col),1)
     expect_equal(colnames(single_col), c("1"))
 })
+
+test_that('read_ods works with all kind of character encodings', {
+  expect_error(read_ods('../testdata/wild_character_encoding.ods', sheet='évaluation'),   NA) # é as e and accent
+  expect_error(read_ods('../testdata/wild_character_encoding.ods', sheet='évaluation 2'), NA) # é as one character
+})
