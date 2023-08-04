aab016e9645b58b097452fb59ef1919b2813c50a
chainsawriot <chainsawtiney@gmail.com>
2023-08-04 14:33:03 +0200

    Replace `utils::unzip`
    Locally, it works okay. Let's see what the CI thinks.

--- R/zip.R
+++ R/zip.R
@@ -2,20 +2,17 @@
 
 # Called only from C++ code, but currently needs to be implemented in R.
 zip_buffer <- function(zip_path, file_path) {
-  files <- utils::unzip(zip_path, list = TRUE)
-
-  indx <- match(file_path, files$Name)
-  if (is.na(indx)) {
-    stop("Couldn't find '", file_path, "' in '", zip_path, "'", call. = FALSE)
-  }
-
-  size <- files$Length[indx]
-
-  con <- unz(zip_path, file_path, open = "rb")
-  on.exit(close(con), add = TRUE)
-  readBin(con, raw(), n = size)
+    files <- zip::zip_list(zip_path)
+    indx <- match(file_path, files$filename)
+    if (is.na(indx)) {
+        stop("Couldn't find '", file_path, "' in '", zip_path, "'", call. = FALSE)
+    }
+    size <- files$uncompressed_size[indx]
+    con <- unz(zip_path, file_path, open = "rb")
+    on.exit(close(con), add = TRUE)
+    readBin(con, raw(), n = size)
 }
 
 zip_has_file <- function(zip_path, file_path) {
-  file_path %in% utils::unzip(zip_path, list = TRUE)$Name
+    file_path %in% zip::zip_list(zip_path)$filename
 }
