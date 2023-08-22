e2b55b51e3e782096b7eda34a66313933588ba0f
chainsawriot <chainsawtiney@gmail.com>
2023-08-22 21:51:13 +0200

    Use a similar style to pbrohan
    Except camelCase...

--- src/write_sheet_.cpp
+++ src/write_sheet_.cpp
@@ -6,7 +6,7 @@
 #include <fstream>
 #include <iostream>
 
-void cell_out_ (const cpp11::r_string& value_type, const cpp11::r_string& value, std::ofstream& xml_file) {
+void cell_out (const cpp11::r_string& value_type, const cpp11::r_string& value, std::ofstream& xml_file) {
     const char* value_type_c = Rf_translateCharUTF8(value_type);
     const char* value_c = Rf_translateCharUTF8(value);
     xml_file << "<table:table-cell office:value-type=\"";
@@ -20,7 +20,7 @@ void cell_out_ (const cpp11::r_string& value_type, const cpp11::r_string& value,
     xml_file << "</text:p></table:table-cell>\n";
 }
 
-void pad_rows_ (const bool& padding, const int& cols, const int& cmax, std::ofstream& xml_file) {
+void pad_rows (const bool& padding, const int& cols, const int& cmax, std::ofstream& xml_file) {
     if (cols < cmax && padding) {
         xml_file << "<table:table-cell table:number-columns-repeated=\"";
         xml_file << cmax - cols;
@@ -28,29 +28,29 @@ void pad_rows_ (const bool& padding, const int& cols, const int& cmax, std::ofst
     }
 }
 
-cpp11::strings dimnames_(const cpp11::data_frame& x, bool cols) {
+cpp11::strings dimnames(const cpp11::data_frame& x, bool cols) {
     // Is there a better way?
     cpp11::function dimnames_rfun = cpp11::package("readODS")[".get_sanitized_dimnames"];
     return cpp11::writable::strings(static_cast<SEXP>(dimnames_rfun(x, cols)));
 }
 
-cpp11::list_of<cpp11::strings> sanitize_(const cpp11::data_frame& x, const cpp11::strings column_types) {
+cpp11::list_of<cpp11::strings> sanitize(const cpp11::data_frame& x, const cpp11::strings column_types) {
     cpp11::function sanitize_rfun = cpp11::package("readODS")[".sanitize_df"];
     return cpp11::writable::list_of<cpp11::strings>(static_cast<SEXP>(sanitize_rfun(x, column_types)));
 }
 
-cpp11::strings get_column_types_(const cpp11::data_frame& x) {
+cpp11::strings get_column_types(const cpp11::data_frame& x) {
     cpp11::function get_column_types_rfun = cpp11::package("readODS")[".get_column_types"];
     return cpp11::writable::strings(static_cast<SEXP>(get_column_types_rfun(x)));
 }
 
-std::string escape_xml_(const std::string& input) {
+std::string escape_xml(const std::string& input) {
     cpp11::sexp input_sexp = cpp11::as_sexp(input);
     cpp11::function escape_xml_rfun = cpp11::package("readODS")[".escape_xml"];
     return cpp11::as_cpp<std::string>(escape_xml_rfun(input_sexp));
 }
 
-void write_empty_(const std::string& escaped_sheet, std::ofstream& xml_file, const std::string& header, const std::string& footer) {
+void write_empty(const std::string& escaped_sheet, std::ofstream& xml_file, const std::string& header, const std::string& footer) {
     xml_file << header;
     xml_file << "<table:table table:name=\"";
     xml_file << escaped_sheet;
@@ -69,21 +69,20 @@ cpp11::r_string write_sheet_(const std::string& filename,
                              const bool padding,
                              const std::string& header,
                              const std::string& footer) {
-    // TODO: if x.nrow() == 0; just write empty xml
     std::ofstream xml_file(filename);
-    std::string escaped_sheet = escape_xml_(sheet);
+    std::string escaped_sheet = escape_xml(sheet);
     if (x.ncol() == 0 || (x.nrow() == 0 && !col_names && x.ncol() != 0)) {
-        write_empty_(escaped_sheet, xml_file, header, footer);
+        write_empty(escaped_sheet, xml_file, header, footer);
         return filename;
     }
-    cpp11::strings column_types = get_column_types_(x);
+    cpp11::strings column_types = get_column_types(x);
     cpp11::strings rownames_x, colnames_x;
-    cpp11::list_of<cpp11::strings> x_list = sanitize_(x, column_types);
+    cpp11::list_of<cpp11::strings> x_list = sanitize(x, column_types);
     if (row_names) {
-        rownames_x = dimnames_(x, false);
+        rownames_x = dimnames(x, false);
     }
     if (col_names) {
-        colnames_x = dimnames_(x, true);
+        colnames_x = dimnames(x, true);
     }
     int rows = col_names ? x_list[0].size() + 1 : x_list[0].size();
     int cols = row_names ? column_types.size() + 1 : column_types.size();
@@ -101,31 +100,31 @@ cpp11::r_string write_sheet_(const std::string& filename,
     if (col_names) {
         xml_file << "<table:table-row table:style-name=\"ro1\">";
         if (row_names) {
-            cell_out_("string", "", xml_file);
+            cell_out("string", "", xml_file);
         }
         for (int j = 0; j < colnames_x.size(); j++) {
-            cell_out_("string", colnames_x[j], xml_file);
+            cell_out("string", colnames_x[j], xml_file);
         }
-        pad_rows_(padding, cols, cmax, xml_file);
+        pad_rows(padding, cols, cmax, xml_file);
         xml_file << "</table:table-row>\n";
     }
     for (int i = 0; i < x_list[0].size(); i++) {
         xml_file << "<table:table-row table:style-name=\"ro1\">\n";
         if (row_names) {
-            cell_out_("string", rownames_x[i], xml_file);
+            cell_out("string", rownames_x[i], xml_file);
         }
         for (int j = 0; j < column_types.size(); j++) {
             if (x_list[j][i] != NA_STRING) {
-                cell_out_(column_types[j], x_list[j][i], xml_file);
+                cell_out(column_types[j], x_list[j][i], xml_file);
                 continue;
             }
             if (!na_as_string) {
                 xml_file << "<table:table-cell/>\n";
                 continue;
             }
-            cell_out_("string", "NA", xml_file);
+            cell_out("string", "NA", xml_file);
         }
-        pad_rows_(padding, cols, cmax, xml_file);
+        pad_rows(padding, cols, cmax, xml_file);
         xml_file << "</table:table-row>\n";
     }
     // pad_columns
